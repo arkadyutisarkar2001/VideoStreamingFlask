d7e9d7b02e4effb0ad144a856a79f6c16316a526
arkadyutisarkar2001 <arkadyutisarkar2001@gmail.com>
2023-06-14 14:40:21 +0530

    #

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ import RPi.GPIO as GPIO
 import serial,time ,random
 import json
 app = Flask(__name__)
-ser = serial.Serial('/dev/ttyACM0',115200,timeout=4)
-value1=0
-value2=0
-
+ser = serial.Serial('/dev/ttyACM1',115200,timeout=4)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(5,GPIO.IN)
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -27,13 +25,16 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 @app.route('/data',methods=['POST','GET'])
 def data():
+    GPIO.setup(5,GPIO.IN,GPIO.PUD_DOWN)
+    global lat,long,temparature,humidity,prevhu,prevt,prevlat,prevlong,prevcpm,prevnSv,nSv,prevstatus,status 
     time.sleep(2)
-    led= False
-    if GPIO.input(5)==GPIO.HIGH:
-        led=True
+    status= False
+    if (GPIO.input(5)==GPIO.HIGH):
+         status=True
+         print("yes")
     else:
-            led=False 
-    global lat,long,temparature,humidity,prevhu,prevt,prevlat,prevlong,prevcpm,prevnSv,nSv        
+         status=False 
+         
     if ser.in_waiting > 0:
         global jsonData
         
@@ -44,13 +45,13 @@ def data():
         except json.decoder.JSONDecodeError:
                 print("Failed to parse JSON")
         humidity = jsonData["humidity"]
-        temparature = jsonData["temparature"]
+        temparature = jsonData["temperature"]
         lat=jsonData["lat"]
         long=jsonData["long"]
         cpm=jsonData["cpm"]
         nSv=jsonData["nSv"]
-        avg_h2s=jsonData["average_of_h2s"]
-        avg_co=jsonData[" average_of_co "]
+        avg_h2s=jsonData["h2sppm"]
+        avg_co=jsonData["coppm"]
         alt=jsonData["alt"]
         prevlat =lat
         prevlong=long
@@ -58,12 +59,13 @@ def data():
         prevt= temparature
         prevcpm=cpm
         prevnSv=nSv
+        
     # store the variable and process it further as needed
         print(f"humidity: { humidity}, temparature: {temparature},lat:{lat},long:{long}")
         print (temparature) 
-        return jsonify({'humidity':humidity,'temparature':temparature,'lat':lat,'long':long,'cpm':cpm,'nSv':nSv,'avg_h2s':avg_h2s,'avg_co':avg_co,'alt':alt,'status':led})
+        return jsonify({'humidity':humidity,'temparature':temparature,'lat':lat,'long':long,'cpm':cpm,'nSv':nSv,'avg_h2s':avg_h2s,'avg_co':avg_co,'alt':alt,'status':status})
     else:
         print(f"humidity::{prevhu},temparature:{prevt},lat:{prevlat},long:{prevlong}")
-        return jsonify({'humidity':prevhu,'temparature':prevt,'lat':prevlat,'long':prevlong,'cpm':prevcpm,'nSv':prevnSv,'avg_h2s':0,'avg_co':0,'alt':0,'status':'false'})
+        return jsonify({'humidity':prevhu,'temparature':prevt,'lat':prevlat,'long':prevlong,'cpm':prevcpm,'nSv':prevnSv,'avg_h2s':0,'avg_co':0,'alt':0,'status':status})
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000',debug=True)
